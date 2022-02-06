4ed1e5c33c0a699c61d8d41e2b76195c7e5d22c8
Dmitrii_IT <74114011+Dmitrii1110@users.noreply.github.com>
2022-02-06 19:18:38 +0300

    Initial Commit

---
+++ F.cars/F_carsApp.swift
@@ -0,0 +1,17 @@
+//
+//  F_carsApp.swift
+//  F.cars
+//
+//  Created by admin1 on 06.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct F_carsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
