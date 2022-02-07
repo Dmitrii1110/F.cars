5d43db00d7e7760774cf82e49fd4654a07744719
Dmitrii_IT <74114011+Dmitrii1110@users.noreply.github.com>
2022-02-07 22:38:57 +0300

    Функция Restart приложения и завершения экрана настроек (Settings View)

--- F.cars/App/SettingsView.swift
+++ F.cars/App/SettingsView.swift
@@ -11,6 +11,7 @@ struct SettingsView: View {
     //MARK: - PROPERTIES
     
     @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
     
     //MARK: - BODY
     var body: some View {
@@ -36,6 +37,34 @@ struct SettingsView: View {
                     }
                     
                     //MARK: - SECTION 2
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Customization", labelImage: "gear")
+                    ) {
+                        Divider().padding(.vertical, 4)
+                        
+                        Text("При желании Вы можете перезапустить приложение, переключив переключатель в этом поле. И Вы снова увидите экран приветствия.")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                        
+                        Toggle(isOn: $isOnboarding) {
+                            if isOnboarding {
+                                Text("Restarted".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.green)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.secondary)
+                            }
+                            
+                        }
+                        .padding()
+                        .background(
+                            Color(UIColor.tertiarySystemBackground).clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous)))
+                    }
                     
                     //MARK: - SECTION 3
                     GroupBox(
